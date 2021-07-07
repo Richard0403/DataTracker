7d2877b3e7a95e8b1b5054bb93f5fd4646d0a177
wuzhiguo <wuzhiguo@ksjgs.com>
2021-07-07 14:54:43 +0800

    添加日志

--- app/src/main/java/com/richard/datatracker/App.kt
+++ app/src/main/java/com/richard/datatracker/App.kt
@@ -3,6 +3,8 @@ package com.richard.datatracker
 import android.app.Application
 import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.richard.tracker.api.OnCommitListener
 import com.richard.tracker.constant.GlobalConfig
@@ -33,12 +35,16 @@ class App: MultiDexApplication() {
             onCommitListener = object : OnCommitListener {
                 override fun commitClickData(clickData: MutableMap<String, Any?>?) {
                     //点击数据
-                    Log.i("commitClickData", "commitClickData==$clickData")
+                    Log.i("commitClickData", "commitClickData==${clickData.toString()}")
                 }
 
                 override fun commitExposureData(exposureData: MutableList<MutableMap<String, Any?>?>) {
                     //曝光数据
-                    Log.i("commitExposureData", "commitExposureData==$exposureData")
+
+                    for (data in exposureData) {
+
+                        Log.i("commitExposureData", "commitExposureData==${JSON.toJSONString(data)}")
+                    }
                 }
             })
     }
